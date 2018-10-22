2d10cfc1f98dfdf2d0504b8e5e007f93bf337b2e
ldc_ <934831065@qq.com>
2018-10-22 10:36:25 +0800

    update ddk search params

--- ddk/goods_search.go
+++ ddk/goods_search.go
@@ -14,7 +14,7 @@ type GoodsSearchParams struct {
 	Page         *int                `json:"page,omitempty"`          //默认值1，商品分页数
 	PageSize     *int                `json:"page_size,omitempty"`     //默认100，每页商品数量
 	SortType     int                 `json:"sort_type"`               //排序方式:0-综合排序;1-按佣金比率升序;2-按佣金比例降序;3-按价格升序;4-按价格降序;5-按销量升序;6-按销量降序;7-优惠券金额排序升序;8-优惠券金额排序降序;9-券后价升序排序;10-券后价降序排序;11-按照加入多多进宝时间升序;12-按照加入多多进宝时间降序;13-按佣金金额升序排序;14-按佣金金额降序排序;15-店铺描述评分升序;16-店铺描述评分降序;17-店铺物流评分升序;18-店铺物流评分降序;19-店铺服务评分升序;20-店铺服务评分降序;27-描述评分击败同类店铺百分比升序，28-描述评分击败同类店铺百分比降序，29-物流评分击败同类店铺百分比升序，30-物流评分击败同类店铺百分比降序，31-服务评分击败同类店铺百分比升序，32-服务评分击败同类店铺百分比降序
-	WithCoupon   string              `json:"with_coupon"`             //是否只返回优惠券的商品，false返回所有商品，true只返回有优惠券的商品
+	WithCoupon   bool                `json:"with_coupon"`             //是否只返回优惠券的商品，false返回所有商品，true只返回有优惠券的商品
 	RangeList    *[]GoodsSearchRange `json:"range_list,omitempty"`    //范围列表，可选值：[{"range_id":0,"range_from":1,"range_to":1500},{"range_id":1,"range_from":1,"range_to":1500}]
 	CatID        *int64              `json:"cat_id,omitempty"`        //商品类目ID，使用pdd.goods.cats.get接口获取
 	GoodsIDList  *[]int64            `json:"goods_id_list,omitempty"` //商品ID列表。例如：[123456,123]，当入参带有goods_id_list字段，将不会以opt_id、 cat_id、keyword维度筛选商品
