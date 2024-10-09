1bd3b7e5ea03834f4ba5c4f41d743c8c5086db66
gms <1418734879@qq.com>
2024-10-09 14:33:06 +0800

    fix: OauthGoodsPromotionUrlGenerateParams一些参数改为非必填

--- ddk_tool/oauth_goods_prom_url_generate.go
+++ ddk_tool/oauth_goods_prom_url_generate.go
@@ -10,25 +10,25 @@ import (
 */
 
 type OauthGoodsPromotionUrlGenerateParams struct {
-	CashGiftId                int64             `json:"cash_gift_id"`                 //多多礼金ID
-	CashGiftName              string            `json:"cash_gift_name"`               // 自定义礼金标题
-	CustomParameters          string            `json:"custom_parameters"`            //自定义参数
-	ForceDuoId                bool              `json:"force_duo_id"`                 //是否使用多多客专属推广计划
-	GenerateAuthorityUrl      bool              `json:"generate_authority_url"`       //是否生成带授权的单品链接。如果未授权，则会走授权流程
-	GenerateMallCollectCoupon bool              `json:"generate_mall_collect_coupon"` //是否生成店铺收藏券推广链接
-	GenerateQqApp             bool              `json:"generate_qq_app"`              //是否生成qq小程序
-	GenerateSchemaUrl         bool              `json:"generate_schema_url"`          //是否返回 schema URL
-	GenerateShortUrl          bool              `json:"generate_short_url"`           //是否生成短链接，true-是，false-否
-	GenerateWeApp             bool              `json:"generate_we_app"`              //是否生成拼多多福利券微信小程序推广信息
-	GoodsSignList             []string          `json:"goods_sign_list"`              //商品goodsSign列表，例如：["c9r2omogKFFAc7WBwvbZU1ikIb16_J3CTa8HNN"]，支持批量生链
-	MaterialId                string            `json:"material_id"`                  //素材ID，可以通过商品详情接口获取商品素材信息
-	MultiGroup                bool              `json:"multi_group"`                  //true--生成多人团推广链接 false--生成单人团推广链接（默认false）1、单人团推广链接：用户访问单人团推广链接，可直接购买商品无需拼团。
-	Pid                       string            `json:"p_id"`                         //推广位ID
-	SearchId                  string            `json:"search_id"`                    //搜索id，建议填写，提高收益。来自pdd.ddk.goods.recommend.get、pdd.ddk.goods.search、pdd.ddk.top.goods.list.query等接口
-	SpecialParams             map[string]string `json:"special_params"`               //特殊参数
-	UrlType                   int               `json:"url_type"`                     //生成商品链接类型 0-默认 1-百补相似品列表
-	ZsDuoId                   int64             `json:"zs_duo_id"`                    //招商多多客ID
-	GenerateWeAppLongLink     bool              `json:"generate_we_app_long_link"`    //是否生成小程序schema长链
+	CashGiftId                *int64            `json:"cash_gift_id,omitempty"`                 //多多礼金ID
+	CashGiftName              *string           `json:"cash_gift_name,omitempty"`               // 自定义礼金标题
+	CustomParameters          *string           `json:"custom_parameters,omitempty"`            //自定义参数
+	ForceDuoId                *bool             `json:"force_duo_id,omitempty"`                 //是否使用多多客专属推广计划
+	GenerateAuthorityUrl      *bool             `json:"generate_authority_url,omitempty"`       //是否生成带授权的单品链接。如果未授权，则会走授权流程
+	GenerateMallCollectCoupon *bool             `json:"generate_mall_collect_coupon,omitempty"` //是否生成店铺收藏券推广链接
+	GenerateQqApp             *bool             `json:"generate_qq_app,omitempty"`              //是否生成qq小程序
+	GenerateSchemaUrl         *bool             `json:"generate_schema_url,omitempty"`          //是否返回 schema URL
+	GenerateShortUrl          *bool             `json:"generate_short_url,omitempty"`           //是否生成短链接，true-是，false-否
+	GenerateWeApp             *bool             `json:"generate_we_app,omitempty"`              //是否生成拼多多福利券微信小程序推广信息
+	GoodsSignList             []string          `json:"goods_sign_list,omitempty"`              //商品goodsSign列表，例如：["c9r2omogKFFAc7WBwvbZU1ikIb16_J3CTa8HNN"]，支持批量生链
+	MaterialId                *string           `json:"material_id,omitempty"`                  //素材ID，可以通过商品详情接口获取商品素材信息
+	MultiGroup                *bool             `json:"multi_group,omitempty"`                  //true--生成多人团推广链接 false--生成单人团推广链接（默认false）1、单人团推广链接：用户访问单人团推广链接，可直接购买商品无需拼团。
+	Pid                       string            `json:"p_id"`                                   //推广位ID
+	SearchId                  *string           `json:"search_id,omitempty"`                    //搜索id，建议填写，提高收益。来自pdd.ddk.goods.recommend.get、pdd.ddk.goods.search、pdd.ddk.top.goods.list.query等接口
+	SpecialParams             map[string]string `json:"special_params,omitempty"`               //特殊参数
+	UrlType                   *int              `json:"url_type,omitempty"`                     //生成商品链接类型 0-默认 1-百补相似品列表
+	ZsDuoId                   *int64            `json:"zs_duo_id,omitempty"`                    //招商多多客ID
+	GenerateWeAppLongLink     *bool             `json:"generate_we_app_long_link,omitempty"`    //是否生成小程序schema长链
 }
 
 type OauthGoodsPromotionUrlGenerateResult struct {
@@ -70,11 +70,11 @@ type GoodsPromotionUrlList struct {
 	WeixinShortLink string `json:"weixin_short_link"`
 }
 
-func (this *DuoduoKeTool) OauthGoodsPromotionUrlGenerate(p *ConvertLinkParams, accessToken string) (*ConvertLinkResult, error) {
+func (this *DuoduoKeTool) OauthGoodsPromotionUrlGenerate(p *OauthGoodsPromotionUrlGenerateParams, accessToken string) (*OauthGoodsPromotionUrlGenerateResult, error) {
 	apiType := `pdd.ddk.oauth.goods.prom.url.generate`
 	params, paramsURL := util.FormatURLParams(p)
 	url := this.GetURL(apiType, accessToken, params, paramsURL)
-	var result ConvertLinkResult
+	var result OauthGoodsPromotionUrlGenerateResult
 	err := util.HttpPOST(url, nil, &result)
 	if err != nil {
 		return nil, err
